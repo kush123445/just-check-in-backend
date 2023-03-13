8043ecb5e81e260a71dfcbfa00b20db24f02b7a2
kushal <kushalhts00@gmail.com>
2023-03-13 21:48:45 +0530

    l

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const app = express();
 var cors = require('cors')
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.use(
     bodyparser.urlencoded({
         extended:true,
